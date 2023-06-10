5e6e1aa48264363ca712f6127e532349c58b6351
BxRayven <110394714+BxRayven@users.noreply.github.com>
2023-06-10 22:54:57 +0100

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,11 @@ client_scripts {
     'client.lua'
 }
 
+server_scripts {
+    'server.lua'
+}
+
 fivem_checker 'yes'
 github 'https://github.com/BxRayven/nohatfalloff'
-version '1.0.0'
+version '2.0.0'
 name '^2No Hat Fall Off'
