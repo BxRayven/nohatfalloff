9ceccb2e4aeb7eeeff813b1a5b6928717595da6c
BxRayven <110394714+BxRayven@users.noreply.github.com>
2023-06-11 12:15:59 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -13,10 +13,30 @@
 
 --====================================================================================================================================================--
 
+local config = {
+    delay = 0, -- Adjust this value to change the delay
+    command = "nohatfalloff", -- Adjust this value to change the command
+    enableHatFunctionality = true -- Adjust this value to enable/disable the hat functionality
+}
+
+local isHatFunctionalityEnabled = true
+
 Citizen.CreateThread(function()
     while true do
         local playerPed = PlayerPedId()
-        SetPedCanLosePropsOnDamage(playerPed, false, 0)
-        Citizen.Wait(0)
+        if isHatFunctionalityEnabled then
+            SetPedCanLosePropsOnDamage(playerPed, false, 0)
+        end
+        Citizen.Wait(config.delay)
+    end
+end)
+
+RegisterCommand(config.command, function(source, args, rawCommand)
+    isHatFunctionalityEnabled = not isHatFunctionalityEnabled
+
+    if isHatFunctionalityEnabled then
+        print("Hat functionality enabled.")
+    else
+        print("Hat functionality disabled.")
     end
 end)
